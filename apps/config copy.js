7c5e2e474bfa171924d23a97bff8a536549af8e9
rian.digitalent <rian.digitalent@gmail.com>
2020-11-18 11:32:43 +0700

    securing apikey

--- apps/config copy.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:admintampan@bcaadts02.xejsv.mongodb.net/BCAA?retryWrites=true&w=majority',
-    AWS_ID: 'AKIAVNHUZ3Q5MEDGTGPJ',
-    AWS_SECRET: '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',
-    BUCKET_NAME: 'dts02bcaa',
-    region: 'ap-southeast-1'
-}
